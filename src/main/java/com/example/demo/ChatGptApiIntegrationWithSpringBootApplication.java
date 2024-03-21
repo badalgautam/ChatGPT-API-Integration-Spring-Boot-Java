f566fc044c7c500365b2fb4615896f3a3b2251b2
badalgautam <bkjaish2021@gmail.com>
2024-03-21 10:47:10 +0530

    Add ChatGPT API Integration with Spring Boot

---
+++ src/main/java/com/example/demo/ChatGptApiIntegrationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptApiIntegrationWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptApiIntegrationWithSpringBootApplication.class, args);
+	}
+
+}
