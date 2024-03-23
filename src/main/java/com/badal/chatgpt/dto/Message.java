5fe09839c71ff2ae490d8f4391725bb595a738e4
badalgautam <bkjaish2021@gmail.com>
2024-03-23 15:28:36 +0530

    All Code Submit

---
+++ src/main/java/com/badal/chatgpt/dto/Message.java
@@ -0,0 +1,14 @@
+package com.badal.chatgpt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String role;
+    private String content;//questions
+}
